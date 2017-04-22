7553506aa03ca4f9b86b4b9dbb3c576dae8ce63f
JP Lorandi <jp.lorandi@cfyar.com>
2017-04-22 19:30:23 -0300

    Added log messages to train_cli

--- train_cli.js
+++ train_cli.js
@@ -2,7 +2,11 @@ const trainer = require('./train');
 require('dotenv').config();
 
 function train() {
-  trainer('./model/demo_rasa.json');
+  trainer('./model/demo_rasa.json').then((response)=> {
+    console.log(`Train complete: ${response}`);
+  }).catch((reason) => {
+    console.error(`Train failed: ${reason}`);
+  });
 }
 
 train();
