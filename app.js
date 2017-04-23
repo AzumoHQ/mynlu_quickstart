5369e7d3f27fc9e965df39d39dd3c44ee0355595
JP Lorandi <jp.lorandi@cfyar.com>
2017-04-23 02:28:39 -0300

    Implemented express + handlebars

--- app.js
+++ app.js
@@ -4,6 +4,11 @@
 
 // add request module to communicate with the HEROKU Add-on service myNLU-RASA
 const request = require('request');
+const express = require('express');
+const bodyParser = require('body-parser');
+const templates = require('express-handlebars');
+Promise = require('bluebird');
+
 
 if (!process.env.PORT) {
   require('dotenv').config();
@@ -11,46 +16,54 @@ if (!process.env.PORT) {
 
 const server_port = process.env.PORT || 8080;
 
-const http = require('http');
+const app = express();
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.engine('handlebars', templates({defaultLayout: 'main'}));
+
+app.set('view engine', 'handlebars');
+app.set('views', __dirname + '/views');
+
+// const http = require('http');
 const fs = require('fs');
-const formidable = require("formidable");
+// const formidable = require("formidable");
 const util = require('util');
-
-const server = http.createServer(function (req, res) {
-  if (req.method.toLowerCase() == 'get') {
-    displayForm(res);
-  } else if (req.method.toLowerCase() == 'post') {
-    processAllFieldsOfTheForm(req, res);
-  }
-
-});
-
-function displayForm(res, response) {
-  fs.readFile('form.html', function (err, data) {
-    res.writeHead(200, {
-      'Content-Type': 'text/html'
-    });
-    if (response) {
-      res.write(data.toString().replace("<%= parsingResult %>", JSON.stringify(response, null, 2)));
-    } else res.write(data.toString().replace("<%= parsingResult %>", ""));
-    res.end();
-  });
-}
-
-function processAllFieldsOfTheForm(req, res) {
-  const form = new formidable.IncomingForm();
-  form.parse(req, function (err, fields, files) {
-    parseSentence(fields.sentence, function(response) {
-      displayForm(res, response)
-    });
-  });
-}
-
-server.listen(server_port);
-console.log(`server listening on ${server_port}`);
 
 const MYNLU_RASA_URL = process.env.MYNLU_RASA_URL;
 const MYNLU_RASA_TOKEN = process.env.MYNLU_RASA_TOKEN;
+
+
+// const server = http.createServer(function (req, res) {
+//   if (req.method.toLowerCase() == 'get') {
+//     displayForm(res);
+//   } else if (req.method.toLowerCase() == 'post') {
+//     processAllFieldsOfTheForm(req, res);
+//   }
+//
+// });
+
+
+function displayForm(req, res) {
+  res.render('home', {});
+}
+
+function processAllFieldsOfTheForm(req, res) {
+  // console.log('res: ', req);
+  // console.log('res.body: ', req.body);
+  parseSentence(req.body.sentence).then((value) => {
+    res.render('home', {value: JSON.stringify(value)});
+  }).catch((error) => {
+    res.render('home', {error: JSON.stringify(error)});
+  });
+
+
+}
+
+app.get('/', displayForm);
+app.post('/', processAllFieldsOfTheForm);
+
+app.listen(server_port);
+console.log(`server listening on ${server_port}`);
 
 /**
  * This function will parse the sentence returning the result
@@ -58,22 +71,24 @@ const MYNLU_RASA_TOKEN = process.env.MYNLU_RASA_TOKEN;
  * @param callback the call back to pass the response from the service
  * @returns {*}
  */
-function parseSentence(sentence, callback) {
+function parseSentence(sentence) {
   // remember to use your own configuration for MYNLU_RASA_URL and MYNLU_RASA_TOKEN
   const url = `${MYNLU_RASA_URL}/parse?token=${MYNLU_RASA_TOKEN}`;
-  return request.post({
-    url: url,
-    json: {
-      q: sentence // this is passed as the body of the POST { "q" : "the sentence to parse" }
-    }
-  }, function(error, response, body) {
-    if (!error && response.statusCode == 200) {
-      // if there is no error and statusCode is 200 every goes fine so call the callback
-      callback(body);
-    } else {
-      // some error has happened so show data to debug.
-      callback({error: error, status: response.statusCode, body: body});
-    }
-  })
+  return new Promise((resolve, reject) => {
+    request.post({
+      url: url,
+      json: {
+        q: sentence // this is passed as the body of the POST { "q" : "the sentence to parse" }
+      }
+    }, function(error, response, body) {
+      if (!error && response.statusCode == 200) {
+        // if there is no error and statusCode is 200 every goes fine so call the callback
+        resolve(body);
+      } else {
+        // some error has happened so show data to debug.
+        reject({error: error, status: response.statusCode, body: body});
+      }
+    })
+  });
 
 }
