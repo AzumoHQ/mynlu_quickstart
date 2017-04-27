e98ef4319d0a91b1431909bd2b5768b6201e033c
JP Lorandi <jp.lorandi@cfyar.com>
2017-04-27 11:25:46 -0300

    Improved model Fixed train output dump

--- train_cli.js
+++ train_cli.js
@@ -3,9 +3,9 @@ require('dotenv').config();
 
 function train() {
   trainer('./model/demo_rasa.json').then((response)=> {
-    console.log(`Train complete: ${response}`);
+    console.log(`Train complete: ${JSON.stringify(response)}`);
   }).catch((reason) => {
-    console.error(`Train failed: ${reason}`);
+    console.error(`Train failed: ${JSON.stringify(reason)}`);
   });
 }
 
