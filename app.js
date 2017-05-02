a7f5c31ff810f09a6f487a1c3b2c18f09e5ef7ae
Alvaro Quezada <aqt@powermedia.cl>
2017-05-02 16:27:48 -0300

    Improvements on the HTML and fix on JSON of reference.π

--- app.js
+++ app.js
@@ -40,13 +40,23 @@ app.post("/", function (req, res) {
         case "train":
           if (req.files.trainFile) {
             const response = yield trainFile(req.files.trainFile.path);
-            renderData = {trainResult: response};
+            renderData = {trainResult: response, json_text: response.json_text};
+          } else if (req.fields.json_text) {
+            const response = yield train(req.fields.json_text);
+            renderData = {trainResul: response, json_text: req.fields.json_text};
+          } else {
+            renderData = {trainResult: "Nothing to parse"};
           }
           break;
         case "parse":
           if (req.fields.sentence) {
-            const response = yield parseSentence(req.fields.sentence);
-            renderData = {parseResult: JSON.stringify(response, null, 2)};
+            try {
+              const response = yield parseSentence(req.fields.sentence);
+              renderData = {parseResult: JSON.stringify(response, null, 2)};
+            } catch (err) {
+              console.log('parse error: ', err);
+              renderData = {parseError: `ERROR ${err.statusCode}: ${err.body}`}
+            }
           }
           break;
         case "status":
@@ -74,7 +84,7 @@ console.log(`server listening on ${server_port}`);
  * @returns {*}
  */
 function parseSentence(sentence) {
-  return new Promise(function (resolve, reject) {
+  return new Promise( (resolve, reject) => {
     const url = `${MYNLU_RASA_URL}/parse?token=${MYNLU_RASA_TOKEN}`;
     request.post({
       url: url,
@@ -89,7 +99,7 @@ function parseSentence(sentence) {
         resolve(body);
       } else {
         // some error has happened so show data to debug.
-        reject({error: error, status: response.statusCode, body: body});
+        reject({error: error, statusCode: response.statusCode, body: body});
       }
     })
   });
@@ -97,22 +107,44 @@ function parseSentence(sentence) {
 }
 
 function trainFile(path) {
-  return new Promise(function (resolve, reject) {
-    const formData = {
-      body: fs.createReadStream(path)
-    };
-    const url = `${MYNLU_RASA_URL}/train?token=${MYNLU_RASA_TOKEN}`;
+  return Promise.coroutine(function *() {
+    const strData = yield new Promise( (resolve, reject)=> {
+      fs.readFile(path, function (err, strBuffer) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(strBuffer)
+        }
+      })
+    });
+    const json_text = strData.toString();
+    const result = yield train(json_text);
+    result.json_text = json_text;
+    return result;
+  })();
+}
 
-    request.post({url, formData}, (err, httpResponse, body) => {
+function train(strData) {
+
+
+  const jsonData = JSON.parse(strData);
+  const url = `${MYNLU_RASA_URL}/train?token=${MYNLU_RASA_TOKEN}`;
+
+  return new Promise(function (resolve, reject){
+    request.post({
+      uri: url,
+      json: jsonData,
+      method: "POST"
+    }, (err, response, body) => {
       console.log("train response");
-      console.log(httpResponse.statusCode);
-      if (!err && httpResponse.statusCode == 200) {
+      console.log(response.statusCode);
+      if (!err && response.statusCode == 200) {
         resolve(body);
       } else {
-        reject({err, httpResponse, body});
+        reject({err, response, body});
       }
     })
-  });
+  })
 }
 
 
